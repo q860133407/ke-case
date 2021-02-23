adc56a4ce99ce6be6603f0532b91b8b1f8f8aff6
zxg <860133407@qq.com>
2021-02-24 00:25:19 +0800

    feat: cas 案例提交

--- src/main/java/com/ke/cas/CasDemo.java
+++ src/main/java/com/ke/cas/CasDemo.java
@@ -10,6 +10,7 @@ public class CasDemo {
 
     public static void main(String[] args) {
         AtomicInteger atomicInteger = new AtomicInteger(5);
-        //atomicInteger.compareAndSet("")
+        System.out.println(atomicInteger.compareAndSet(5, 2019)+"\t current data:"+atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(5, 1024)+"\t current data:"+atomicInteger.get());
     }
 }
