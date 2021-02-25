6c6b136a46017a307a6aa97bad70e4d19a5cb332
zxg <860133407@qq.com>
2021-02-25 23:27:17 +0800

    feat: ABA 问题案例，原子引用案例

---
+++ src/main/java/com/ke/cas/ABADemo.java
@@ -0,0 +1,33 @@
+package com.ke.cas;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class User{
+    String userName;
+    int age;
+}
+
+/**
+ * @author zxg_QAQ
+ * @date 2021/2/25下午11:13
+ */
+public class ABADemo {
+
+    public static void main(String[] args) {
+        User user01 = new User("z3",12);
+        User user02 = new User("li4",13);
+
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+        atomicReference.set(user01);
+
+        System.out.println(atomicReference.compareAndSet(user01, user02)+"\t"+atomicReference.get().toString());
+        System.out.println(atomicReference.compareAndSet(user01, user02)+"\t"+atomicReference.get().toString());
+    }
+}
