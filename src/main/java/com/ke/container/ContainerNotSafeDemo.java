6c80f81e008c59cc666be752418ce526d001fb7a
zxg <860133407@qq.com>
2021-02-28 14:04:35 +0800

    feat: ABA 问题案例，集合安全解决

--- src/main/java/com/ke/container/ContainerNotSafeDemo.java
+++ src/main/java/com/ke/container/ContainerNotSafeDemo.java
@@ -1,9 +1,6 @@
 package com.ke.container;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author zxg_QAQ
@@ -13,7 +10,7 @@ import java.util.UUID;
 public class ContainerNotSafeDemo {
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
         for(int i=1;i<=30;i++) {
             new Thread(()->{
                 list.add(UUID.randomUUID().toString().substring(0,8));
