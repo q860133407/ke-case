1b139cbbd0593a75310a76e2a18cedd4adf53ce7
zxg <860133407@qq.com>
2021-02-28 15:08:14 +0800

    feat: ABA 问题案例，集合安全解决

--- src/main/java/com/ke/container/ContainerNotSafeDemo.java
+++ src/main/java/com/ke/container/ContainerNotSafeDemo.java
@@ -1,16 +1,47 @@
 package com.ke.container;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
- * @author zxg_QAQ
+ * @author zxg_QAQ···
  * @date 2021/2/26下午11:05
  * 集合类不安全问题
+ * ArrayList
+ * HashSet
  */
 public class ContainerNotSafeDemo {
 
     public static void main(String[] args) {
-        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        mapNotSafe();
+        setNotSafe();
+        listNotSafe();
+    }
+
+    private static void mapNotSafe() {
+        Map<String,String> map = new ConcurrentHashMap<>();
+        for(int i=1;i<=30;i++) {
+            new Thread(()->{
+                map.put(Thread.currentThread().getName(),UUID.randomUUID().toString().substring(0,8));
+                System.out.println(map);
+            },String.valueOf(i)).start();
+        }
+    }
+
+    private static void setNotSafe() {
+        Set<String> set = new CopyOnWriteArraySet<>();
+        for(int i=1;i<=30;i++) {
+            new Thread(()->{
+                set.add(UUID.randomUUID().toString().substring(0,8));
+                System.out.println(set);
+            },String.valueOf(i)).start();
+        }
+    }
+
+    private static void listNotSafe() {
+        List<String> list = new CopyOnWriteArrayList<>();
         for(int i=1;i<=30;i++) {
             new Thread(()->{
                 list.add(UUID.randomUUID().toString().substring(0,8));
