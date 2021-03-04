c7e100d6e5ca125536a7e57adc0c89fe235908b4
zxg <860133407@qq.com>
2021-03-05 00:57:27 +0800

    feat: BlockingQueue 案例

---
+++ src/main/java/com/ke/queue/BlockingQueueDemo.java
@@ -0,0 +1,13 @@
+package com.ke.queue;
+
+/**
+ * @author zxg_QAQ
+ * @date 2021/3/4下午11:35
+ * 阻塞队列
+ */
+public class BlockingQueueDemo {
+
+    public static void main(String[] args) {
+        
+    }
+}
