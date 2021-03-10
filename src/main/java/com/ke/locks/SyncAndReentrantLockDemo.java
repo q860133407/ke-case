1128d131c24049c840a0c56c0ce676abcb66199c
zxg <860133407@qq.com>
2021-03-10 22:17:55 +0800

    feat: SyncAndReentrantLockDemo

---
+++ src/main/java/com/ke/locks/SyncAndReentrantLockDemo.java
@@ -0,0 +1,14 @@
+package com.ke.locks;
+
+/**
+ * @author zxg_QAQ
+ * @date 2021/3/10下午9:56
+ */
+public class SyncAndReentrantLockDemo {
+
+    public static void main(String[] args) {
+        synchronized (new Object()) {
+
+        }
+    }
+}
