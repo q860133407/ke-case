e1212f48446eba1b6e5a155e05f2ee554d180927
zxg <860133407@qq.com>
2021-03-13 00:39:59 +0800

    feat: ThreadPool 案例

---
+++ src/main/java/com/ke/threadpool/ThreadPoolDemo.java
@@ -0,0 +1,29 @@
+package com.ke.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author zxg_QAQ
+ * @date 2021/3/12下午11:59
+ * 第 4 种获得/使用 java 多线程的方式，线程池
+ */
+public class ThreadPoolDemo {
+
+    public static void main(String[] args) {
+        //ExecutorService threadPool = Executors.newFixedThreadPool(5); // 可处理 5 个线程
+        //ExecutorService threadPool = Executors.newSingleThreadExecutor(); // 只处理一个线程
+        ExecutorService threadPool = Executors.newCachedThreadPool(); // 可处理 N 个线程，动态调整
+        try {
+            for (int i=1;i<=10;i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName()+"\t 线程执行");
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+}
