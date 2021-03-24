b70774990d06d104251718eda3018a91236d5163
zxg <860133407@qq.com>
2021-03-24 23:12:48 +0800

    feat: OOM 案例

---
+++ src/main/java/com/ke/jvm/GCDemo.java
@@ -0,0 +1,26 @@
+package com.ke.jvm;
+
+import java.util.Random;
+
+/**
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseSerialGC
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParNewGC
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParallelGC
+ * @author zxg_QAQ
+ * @date 2021/3/24下午10:26
+ */
+public class GCDemo {
+
+    public static void main(String[] args) {
+        System.out.println("******GCDemo");
+        try {
+            String str = "test";
+            while (true) {
+                str += str + new Random().nextInt(7777777) + new Random().nextInt(888888888);
+                str.intern();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
