7630844e4be1b67771d81fac50f607c017abca44
zxg <860133407@qq.com>
2021-04-04 18:46:13 +0800

    feat: GC垃圾回收器配置案例

---
+++ src/main/java/com/ke/algorithm/TwoSumDemo.java
@@ -0,0 +1,12 @@
+package com.ke.algorithm;
+
+/**
+ * @author zxg_QAQ
+ * @date 2021/4/3下午10:05
+ */
+public class TwoSumDemo {
+
+    public static void main(String[] args) {
+        int [] sum = new int[]{};
+    }
+}
