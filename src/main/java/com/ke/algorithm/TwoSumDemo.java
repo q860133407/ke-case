41d2d6ca072eaf8f3fc0364e4c95e46a8ecc8bec
zxg <860133407@qq.com>
2021-04-06 23:12:55 +0800

    feat: GC垃圾回收器配置案例

--- src/main/java/com/ke/algorithm/TwoSumDemo.java
+++ src/main/java/com/ke/algorithm/TwoSumDemo.java
@@ -7,6 +7,15 @@ package com.ke.algorithm;
 public class TwoSumDemo {
 
     public static void main(String[] args) {
-        int [] sum = new int[]{123};
+        int [] sum = new int[]{2,7,11,15};
+        int target = 9;
+    }
+
+    public int[] twoSum1(int[] nums,int target) {
+        for (int i = 0;i < nums.length;i++) {
+
+        }
+
+        return null;
     }
 }
