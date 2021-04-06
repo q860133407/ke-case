0cb1849d3a42fd28a0b928077af7dde88fe8b5ec
zxg <860133407@qq.com>
2021-04-07 00:01:16 +0800

    feat: 两数之和，最优写法

--- src/main/java/com/ke/algorithm/TwoSumDemo.java
+++ src/main/java/com/ke/algorithm/TwoSumDemo.java
@@ -1,5 +1,8 @@
 package com.ke.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zxg_QAQ
  * @date 2021/4/3下午10:05
@@ -8,20 +11,36 @@ public class TwoSumDemo {
 
     public static void main(String[] args) {
         int [] numbers01 = new int[]{2,7,11,15};
-        int target = 22;
+        int target = 9;
         int [] numbers02 = twoSum1(numbers01,target);
         for (int i=0;i<numbers02.length;i++) {
             System.out.println(numbers02[i]);
         }
+        int [] numbers03 = twoSum2(numbers01,target);
+        for (int i=0;i<numbers03.length;i++) {
+            System.out.println(numbers03[i]);
+        }
     }
 
-    public static int[] twoSum1(int[] nums,int target) {
-        for (int i = 0;i < nums.length;i++) {
-            for (int j = i+1; j < nums.length;j++) {
-                if (target - nums[i] == nums[j]) {
+    private static int[] twoSum1(int[] numbers,int target) {
+        for (int i = 0;i < numbers.length;i++) {
+            for (int j = i+1; j < numbers.length;j++) {
+                if (target - numbers[i] == numbers[j]) {
                     return new int[]{i,j};
                 }
             }
+        }
+        return null;
+    }
+
+    private static int[] twoSum2(int[] numbers,int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i=0;i<numbers.length;i++) {
+            int partnerNumber = target - numbers[i];
+            if (map.containsKey(partnerNumber)) {
+                return new int[] {map.get(partnerNumber),i};
+            }
+            map.put(numbers[i],i);
         }
         return null;
     }
