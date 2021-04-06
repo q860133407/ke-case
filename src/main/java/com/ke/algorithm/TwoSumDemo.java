3b2996aa105a9c74294285cce3cdc1d874f342a3
zxg <860133407@qq.com>
2021-04-06 23:46:23 +0800

    feat: 两数之和，暴力破解

--- src/main/java/com/ke/algorithm/TwoSumDemo.java
+++ src/main/java/com/ke/algorithm/TwoSumDemo.java
@@ -7,15 +7,22 @@ package com.ke.algorithm;
 public class TwoSumDemo {
 
     public static void main(String[] args) {
-        int [] sum = new int[]{2,7,11,15};
-        int target = 9;
+        int [] numbers01 = new int[]{2,7,11,15};
+        int target = 22;
+        int [] numbers02 = twoSum1(numbers01,target);
+        for (int i=0;i<numbers02.length;i++) {
+            System.out.println(numbers02[i]);
+        }
     }
 
-    public int[] twoSum1(int[] nums,int target) {
+    public static int[] twoSum1(int[] nums,int target) {
         for (int i = 0;i < nums.length;i++) {
-
+            for (int j = i+1; j < nums.length;j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{i,j};
+                }
+            }
         }
-
         return null;
     }
 }
